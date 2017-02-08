d5708c936b30362e6c118ec582deca8d9c34b21f
b5710546232 <nattapat.su@ku.th>
2017-02-08 11:40:58 +0700

    fix drop but jump is bug

--- Assets/Scripts/PlayerFootController.cs
+++ Assets/Scripts/PlayerFootController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerFootController : MonoBehaviour {
 	public bool grounded;
+    public bool drop;
 	// Use this for initialization
 	void Start () {
 		grounded = false;
+        drop = true;
 		
 	}
 	
@@ -21,12 +23,60 @@ public class PlayerFootController : MonoBehaviour {
 
     void OnCollisionStay2D(Collision2D other)
     {
-        if (other.gameObject.tag == "Ground")
+
+        float RectHeightObj = other.collider.bounds.size.y;
+        float upObj = other.collider.bounds.center.y + RectHeightObj/2;
+
+        float footHeight = GetComponent<BoxCollider2D>().size.y;
+        float downFoot = GetComponent<BoxCollider2D>().bounds.center.y - footHeight/2;
+        
+      if (other.gameObject.tag == "Ground" || other.gameObject.tag == "AirFloor" )
+        {
+            grounded = true;
+        }
+        
+        if(other.gameObject.tag == "AirFloor"){
+            if(downFoot>=upObj)
+            drop = true;
+        }
+        if(other.gameObject.tag == "Ground"){
+            drop = false;
+        }
+
+    }
+
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        
+         if (other.gameObject.tag == "Ground" || other.gameObject.tag == "AirFloor" )
         {
 
             grounded = true;
         }
+        
+        if(other.gameObject.tag == "AirFloor"){
+            drop = true;
+        }
+        if(other.gameObject.tag == "Ground"){
+            drop = false;
+        }
 
+    }
+
+   
+    void OnTriggerEnter2D(Collider2D other)
+    {
+          
+           if (other.gameObject.tag == "Ground")
+        {
+
+            grounded = false;
+        }
+         if(other.gameObject.tag == "AirFloor"){
+            grounded = false;
+            drop = false;
+        }
     }
 
     void OnCollisionExit2D(Collision2D other)
@@ -36,7 +86,10 @@ public class PlayerFootController : MonoBehaviour {
         {
 
             grounded = false;
-            
+        }
+         if(other.gameObject.tag == "AirFloor"){
+            grounded = false;
+            drop = false;
         }
     }
 

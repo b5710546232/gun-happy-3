b0804a7b9a24c0f09eb9aa4c8cd216307883ceb1
b5710546232 <nattapat.su@ku.th>
2017-02-11 02:23:30 +0700

    add file cbt

---
+++ Assets/Scripts/CombatTextManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatTextManager : MonoBehaviour {
+
+	private CombatTextManager instance;
+	public float speed;
+	public GameObject textPrefab;
+	public RectTransform canvasTransform;
+	public float fadeTime;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public CombatTextManager GetInstance(){
+		if(instance==null){
+			return new CombatTextManager();
+		}
+		return instance;
+	}
+
+	public void CreateText(Vector3 position, string text, Color color,bool critical){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

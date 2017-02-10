940bc336448ef95ba5cd26ddda0a2098b902a038
b5710546232 <nattapat.su@ku.th>
2017-02-11 03:49:21 +0700

    fading text

--- Assets/Scripts/CombatText.cs
+++ Assets/Scripts/CombatText.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CombatText : MonoBehaviour {
 
@@ -16,9 +17,28 @@ public class CombatText : MonoBehaviour {
 		transform.Translate(direction*translation);
 	}
 
-	public void Init(float speed,Vector3 direction)
+	public void Init(float speed,Vector3 direction,float fadetime)
 	{
 		this.speed = speed;
 		this.direction = direction;
+		this.fadetime = fadetime;
+		StartCoroutine("FadeOut");
+	}
+
+	private IEnumerator FadeOut(){
+
+		float startAlpha = GetComponent<Text>().color.a;
+
+		float rate = 1.0f/ fadetime;
+		float progress = 0.0f;
+
+		while(progress< 1.0){
+			Color tmpColor = GetComponent<Text>().color;
+
+			GetComponent<Text>().color = new Color(tmpColor.r,tmpColor.g,tmpColor.b,Mathf.Lerp(startAlpha,0,progress));
+			progress += rate*Time.deltaTime;
+
+			yield return null;
+		}
 	}
 }

56c81b45786c0290632441b812f4902a38f0e171
b5710546232 <nattapat.su@ku.th>
2017-02-10 08:50:51 +0700

    bullet effect

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -15,12 +15,24 @@ public class GunController : MonoBehaviour {
     public float padding_y = -0.1f;
 	public float pos_z = 1;
 
+	public GameObject effect_shot;
+
 
 	
 	// Use this for initialization
 	void Start () {
 		lastBulletShotAt = 0;
 		transform.localPosition = new Vector3(padding_x, padding_y, pos_z);
+
+		if(effect_shot==null){
+			effect_shot = Instantiate(effect_shot);
+			effect_shot.SetActive(false);
+		}else{
+			effect_shot = Instantiate(effect_shot);
+			effect_shot.SetActive(false);
+		}
+		effect_shot.transform.parent = transform;
+		effect_shot.transform.localPosition = new Vector2(0.203f,-0.012f);
 	}
 
 
@@ -42,6 +54,11 @@ public class GunController : MonoBehaviour {
 		
 		BulletController bulletController = gameobj.GetComponent<BulletController>();
 		bulletController.SetShooter(shooter);
+		bulletController.transform.localPosition = new Vector2(-1.023f,-0.615f);
+
+		// effect_shot.transform.localPosition = new Vector3 (transform.position.x + 0.2f,transform.position.y);
+		effect_shot.SetActive(true);
+		effect_shot.GetComponent<Animator>().Play("effect_shot", -1, 0f);
 
 
 	}

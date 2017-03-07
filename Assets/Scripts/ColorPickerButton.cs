ba6a6dee10b314bd4778da7402db84dee57cce3a
b5710546232 <nattapat.su@ku.th>
2017-03-07 17:35:49 +0700

    change color

---
+++ Assets/Scripts/ColorPickerButton.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorPickerButton : MonoBehaviour {
+
+	public GameObject colorPicker;
+	public GameObject player;
+	public GameObject panel;
+	public GameObject c;
+	public List<GameObject> colors;
+	// Use this for initialization
+	public bool toggle;
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+			toggle = false;
+
+		// set to not active
+	
+		
+		colorPicker = GameObject.FindGameObjectWithTag("ColorPicker");
+
+        colors = new List<GameObject>();
+
+		for (int i = 0 ;i< colorPicker.transform.childCount ;i++) {
+			colors.Add(colorPicker.transform.GetChild(i).gameObject);
+		}
+     
+	 	colorPicker.SetActive(false);
+		panel.SetActive(false);
+	}
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ShowColorPicker(){
+
+		colorPicker.SetActive(true);
+		panel.SetActive(true);
+
+		for (int i = 0 ;i< colorPicker.transform.childCount ;i++){
+			colors[i].GetComponent<ColorSelectorButton>().targetPlayer = player;
+
+		}
+		
+		for (int i = 0 ;i< colorPicker.transform.childCount ;i++) {
+
+			colors[i].gameObject.SetActive(true);
+		}
+	}
+}
---
+++ Assets/Scripts/ColorSelectorButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorSelectorButton : MonoBehaviour {
+	public GameObject pane;
+	public GameObject targetPlayer;
+	public GameObject colorPicker;
+	public List<GameObject> colors;
+	// Use this for initialization
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		colorPicker = GameObject.FindGameObjectWithTag("ColorPicker");
+
+        colors = new List<GameObject>();
+
+		for (int i = 0 ;i< colorPicker.transform.childCount ;i++) {
+			colors.Add(colorPicker.transform.GetChild(i).gameObject);
+		}
+	}
+	void Start () {
+		pane = GameObject.FindGameObjectWithTag("Panel");		
+	}
+
+	public void ChooseColor(){
+		targetPlayer.GetComponent<SpriteRenderer>().color = gameObject.GetComponent<Image>().color;
+		
+		pane.SetActive(false);
+		
+
+		for (int i = 0 ;i< colorPicker.transform.childCount ;i++) {
+
+			colors[i].gameObject.SetActive(false);
+		}
+	}
+	
+
+}
